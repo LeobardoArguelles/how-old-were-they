b178338006b62828d8bbce4f810795e3f07481b3
Leobardo Argüelles <leobardomed@gmail.com>
2022-01-20 10:19:26 -0800

    Fix spacing

--- app/main.py
+++ app/main.py
@@ -5,12 +5,20 @@ from app import searcher
 
 bp = Blueprint('main', __name__)
 
+
 @bp.route('/', methods=['POST','GET'])
 def index():
-  if request.method == 'POST':
-    media_type = request.form['watching']
-    title = request.form['movie']
-    persons, real_title = searcher.search(title, media_type)
-    return render_template('result.html', persons=list(persons.values())[:10], title=real_title, is_movie=True if media_type == "movie" else False)
-  return render_template('index.html')
+    """
+    Página principal. Permite buscar una serie o película para conocer
+    la edad de sus actores y actricess.
+    """
+    if request.method == 'POST':
+        media_type = request.form['watching']
+        title = request.form['movie']
+        persons, real_title = searcher.search(title, media_type)
 
+        if persons is None:
+            # No se encontró la película
+            return render_template('notfound.html', title=title)
+        return render_template('result.html', persons=list(persons.values())[:10], title=real_title, is_movie=True if media_type == "movie" else False)
+    return render_template('index.html')
